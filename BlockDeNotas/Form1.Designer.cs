2175a11455511f7635f18378d52ff25bd3c63c53
Francisco Jav <franciscojavierurbm123@gmail.com>
2022-03-16 14:48:41 -0600

    subida

--- BlockDeNotas/Form1.Designer.cs
+++ BlockDeNotas/Form1.Designer.cs
@@ -35,6 +35,15 @@
             this.txtTexto = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,11 +60,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 87);
+            this.dataGridView1.Location = new System.Drawing.Point(449, 77);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(950, 340);
+            this.dataGridView1.Size = new System.Drawing.Size(516, 350);
             this.dataGridView1.TabIndex = 10;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
@@ -85,7 +94,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(796, 38);
+            this.button2.Location = new System.Drawing.Point(890, 45);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 14;
@@ -95,7 +104,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(674, 38);
+            this.button3.Location = new System.Drawing.Point(890, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 15;
@@ -103,11 +112,115 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(790, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Vista en cuadricula";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(15, 77);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(435, 350);
+            this.treeView1.TabIndex = 18;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(669, 48);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Vista en ramas";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Rojo",
+            "Blanco",
+            "Azul",
+            "Negro"});
+            this.comboBox1.Location = new System.Drawing.Point(628, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 21;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(636, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Cambiar color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(483, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 16);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Cambiar fuente";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(480, 44);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 23);
+            this.button6.TabIndex = 24;
+            this.button6.Text = "Cambiar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(353, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Cambiar fondo";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Rojo",
+            "Blanco",
+            "Azul",
+            "Negro"});
+            this.comboBox2.Location = new System.Drawing.Point(329, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 26;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(977, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.txtTexto);
@@ -133,6 +246,15 @@
         private System.Windows.Forms.TextBox txtTexto;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- BlockDeNotas/Form1.cs
+++ BlockDeNotas/Form1.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace BlockDeNotas
     public partial class Form1 : Form
     {
         INotaService activoServices;
+
         public Form1(INotaService ActivoServices)
         {
             this.activoServices = ActivoServices;
@@ -29,28 +31,45 @@ namespace BlockDeNotas
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-           
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             dataGridView1.DataSource = activoServices.Read();
-
-            txtTulo.Visible = false;    
-            txtTexto.Visible = false; 
+            button6.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            comboBox1.Visible = false;
+            txtTulo.Visible = false;
+            txtTexto.Visible = false;
             button2.Visible = false;
             button3.Visible = false;
             dataGridView1.Visible = false;
+            label2.Visible = false;
+            comboBox2.Visible = false;
+            
+
+            button5.Visible = false;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             dataGridView1.Visible = false;
             txtTexto.Visible = true;
-            txtTulo.Visible=true;
-            button2.Visible=true;
-            button3.Visible=true;
-           
+            txtTulo.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            treeView1.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            comboBox1.Visible = true;
+            button6.Visible=true;
+            label4.Visible=true;
+            label5.Visible=true;
+            label2.Visible=true;
+            comboBox2.Visible=true; 
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -65,14 +84,17 @@ namespace BlockDeNotas
 
                 Nota activo = new Nota()
                 {
-                 Id =1,
-                 Titulo = txtTulo.Text,
-                 Texto = txtTexto.Text,
+                    Id = 1,
+                    Titulo = txtTulo.Text,
+                    Texto = txtTexto.Text,
                 };
                 activoServices.Add(activo);
                 dataGridView1.DataSource = null;
                 limpiar();
                 dataGridView1.DataSource = activoServices.Read();
+
+                treeView1.Nodes.Add(txtTulo.Text);
+
 
             }
         }
@@ -81,13 +103,13 @@ namespace BlockDeNotas
 
         private void limpiar()
         {
-            txtTulo.Text ="";
+            txtTulo.Text = "";
             txtTexto.Text = "";
         }
 
         private bool verificar()
         {
-            if (txtTulo.Text == "" || txtTexto.Text== "")
+            if (txtTulo.Text == "" || txtTexto.Text == "")
             {
 
                 return false;
@@ -97,22 +119,132 @@ namespace BlockDeNotas
 
         private void button3_Click(object sender, EventArgs e)
         {
-            txtTexto.Visible=false;
-            txtTulo.Visible=false;
+            txtTexto.Visible = false;
+            txtTulo.Visible = false;
             dataGridView1.Visible = false;
-            button2.Visible=false;
-            button3.Visible=false;
-           
+            button2.Visible = false;
+            button3.Visible = false;
+            treeView1.Visible = true;
+            button4.Visible = true;
+            comboBox1.Visible = false;
+            button6.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label2.Visible = false;
+            comboBox2.Visible = false;
 
         }
 
         private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
         {
-             if (e.RowIndex >= 0)
+            if (e.RowIndex >= 0)
             {
                 Form2 depreciacion = new Form2(activoServices.Read()[e.RowIndex]);
                 depreciacion.ShowDialog();
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            treeView1.Visible = false;
+            button4.Visible = false;
+
+            button5.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            treeView1.Visible = true;
+            dataGridView1.Visible = false;
+            button5.Visible = false;
+           
+            button4.Visible = true;
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0) 
+            {
+              txtTexto.ForeColor = Color.Red;
+              txtTulo.ForeColor = Color.Red;
+            }
+             
+            if(comboBox1.SelectedIndex == 1)
+            {
+                   
+                txtTexto.ForeColor = Color.White; 
+                txtTulo.ForeColor = Color.White;
+                }
+                
+             if (comboBox1.SelectedIndex == 2)
+                       
+             {
+                 txtTexto.ForeColor = Color.Blue;
+                 txtTulo.ForeColor = Color.Blue;
+                 }
+
+            if (comboBox1.SelectedIndex == 3)
+            {
+                txtTexto.ForeColor = Color.Black;
+                txtTulo.ForeColor = Color.Black;
+            }
+                
+                                  
+               
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FontDialog myFontDialog = new FontDialog();
+            if (myFontDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Set the control's font.
+                txtTexto.Font = myFontDialog.Font;
+                txtTulo.Font= myFontDialog.Font;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox2.SelectedIndex == 0) 
+            {
+
+                txtTexto.BackColor = Color.Red;
+                txtTulo.BackColor = Color.Red;
+            }
+            if (comboBox2.SelectedIndex == 1)
+            {
+
+                txtTexto.BackColor = Color.White;
+                txtTulo.BackColor = Color.White;
+            }
+            if(comboBox2.SelectedIndex == 2) 
+            {
+                txtTexto.BackColor = Color.Blue;
+                txtTulo.BackColor = Color.Blue;
+            }
+            if(comboBox2.SelectedIndex == 3)
+            {
+                txtTexto.BackColor = Color.Black;
+                txtTulo.BackColor = Color.Black;
+            }
+        }
     }
 }
+
